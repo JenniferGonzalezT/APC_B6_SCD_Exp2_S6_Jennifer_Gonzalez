b22c3563b2a86ebedda1bb84035eb772d3322592
JenniferGonzalezT <je.gonzalezt@duocuc.cl>
2026-04-17 18:12:25 -0400

    Modifico LoginController

--- backend/src/main/java/com/duoc/backend/LoginController.java
+++ backend/src/main/java/com/duoc/backend/LoginController.java
@@ -5,6 +5,7 @@ import com.duoc.backend.user.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder; // IMPORTANTE AGREGAR ESTO
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,21 +20,19 @@ public class LoginController {
     @Autowired
     private MyUserDetailsService userDetailsService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("login")
     public String login(@RequestBody User loginRequest) {
 
-        /**
-        * En el ejemplo no se realiza la correcta validación del usuario
-        */
-
         final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
 
-        if (!userDetails.getPassword().equals(loginRequest.getPassword())) {
+        if (!passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword())) {
             throw new RuntimeException("Invalid login");
         }
 
         String token = jwtAuthtenticationConfig.getJWTToken(loginRequest.getUsername());
         return token;
     }
-
 }
